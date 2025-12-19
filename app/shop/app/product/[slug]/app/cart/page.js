2a2e79cdf3ae0866d98e91b4904967971157154d
b346901-hash <b346901@gmail.com>
2025-12-19 18:30:43 +0530

    Create page.js

---
+++ app/shop/app/product/[slug]/app/cart/page.js
@@ -0,0 +1,11 @@
+export default function Cart() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h2>Your Cart</h2>
+      <p>1 Plant added</p>
+      <a href="/checkout">
+        <button>Proceed to Checkout</button>
+      </a>
+    </main>
+  );
+}
