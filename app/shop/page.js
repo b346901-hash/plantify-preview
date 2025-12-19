4f7c3599cfd6784ba7f11e8124652bf8f034c300
b346901-hash <b346901@gmail.com>
2025-12-19 18:29:56 +0530

    Create page.js

---
+++ app/shop/page.js
@@ -0,0 +1,13 @@
+export default function Shop() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h2>Shop Plants</h2>
+
+      <div>
+        <a href="/product/snake-plant">Snake Plant</a><br />
+        <a href="/product/money-plant">Money Plant</a><br />
+        <a href="/product/areca-palm">Areca Palm</a>
+      </div>
+    </main>
+  );
+}
